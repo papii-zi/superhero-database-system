8db51f5dcded2ad1fd2c82d5d413b4281debc84a
AphiweShabalala <Shabalalaaphiwe64@gmail.com>
2025-10-13 19:56:56 +0200

    Update Program.cs

--- SuperheroDatabase/Program.cs
+++ SuperheroDatabase/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SuperheroDatabase;
 
 namespace SuperheroDatabase
 {
@@ -16,7 +17,7 @@ namespace SuperheroDatabase
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+           Application.Run(new Superheroesfrm());
         }
     }
 }
